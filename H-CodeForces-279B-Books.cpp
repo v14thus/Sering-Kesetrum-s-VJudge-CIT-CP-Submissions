bfc033198d8af9a39e90569841506dacf8416b7f
viathus <nonplayableratz@gmail.com>
2026-09-19 23:10:31 +0700

    Create H-CodeForces-279B-Books.cpp

---
+++ H-CodeForces-279B-Books.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    long long t;
+    if (!(cin >> n >> t)) return 0;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int left = 0;
+    long long current_time = 0;
+    int max_books = 0;
+
+    for (int right = 0; right < n; right++) {
+        current_time += a[right]; 
+
+        while (current_time > t) {
+            current_time -= a[left];
+            left++;
+        }
+
+        max_books = max(max_books, right - left + 1);
+    }
+
+    cout << max_books << endl;
+}
